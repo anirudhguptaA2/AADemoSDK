35eed75113353d3f35c69336120f7084ae2305b1
Anirudh Gupta <anirudh.gupta2004@gmail.com>
2019-06-01 17:28:55 +0530

    Uploading First demo SDK

---
+++ AADemoSDK/AADemoSDK.h
@@ -0,0 +1,19 @@
+//
+//  AADemoSDK.h
+//  AADemoSDK
+//
+//  Created by Anirudh Gupta on 01/06/19.
+//  Copyright © 2019 Anirudh Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AADemoSDK.
+FOUNDATION_EXPORT double AADemoSDKVersionNumber;
+
+//! Project version string for AADemoSDK.
+FOUNDATION_EXPORT const unsigned char AADemoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AADemoSDK/PublicHeader.h>
+
+
---
+++ AADemoSDK/Controllers/DashboardViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DashboardViewController.swift
+//  AADemoSDK
+//
+//  Created by Anirudh Gupta on 01/06/19.
+//  Copyright © 2019 Anirudh Gupta. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func clickEvent(_ sender: Any) {
+        let alert = UIAlertController.init(title: "Success!", message: "SDK Working Properly", preferredStyle: .alert)
+        alert.addAction(UIAlertAction.init(title: "OK", style: .default, handler: { (action: UIAlertAction) in
+//            self.dismiss(animated: true, completion: nil)
+        }))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
